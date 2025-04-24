d82b595a55f40a04c828bb114700ebf3423c3646
Kayleen Do <kayleendo8@gmail.com>
2025-04-24 18:14:39 -0400

    created transition between first and second scene

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
+import introScene1 from './components/Intro-Page-1.png';
+import introScene2 from './components/Intro-Page-2.png';
+import closet from './components/Closet.png';
+import {useEffect, useState} from 'react';
 import './App.css';
 
 function App() {
+  const images = [introScene1, introScene2];
+  const [index, setIndex] = useState(0);
+  const [clicked, setClicked] = useState(false);
+
+  //auto switches between two intro scenes
+  useEffect(() => {
+    if (clicked) return;
+
+    const interval = setInterval(() => {
+      setIndex(prev => (prev + 1) % images.length)
+    }, 600);
+    
+    return () => clearInterval(interval);
+  }, [clicked])
+
+  const handleClick = () => {
+    setClicked(true);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="Miffy-scene" onClick={handleClick}>
+      <img src={clicked ? closet : images[index]} alt="scenes"/>
     </div>
   );
 }
